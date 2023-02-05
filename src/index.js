dfab6def467ecb0b507ccefb3b00d51143d82a16
sarao <saraok@metropolia.fi>
2023-02-05 18:30:16 +0200

    week2-task2

--- src/index.js
+++ src/index.js
@@ -1,173 +1,39 @@
-//3
-const highNumber = 100;
-const lowNumber = 1;
+const menus = [
+  {name: 'Lingonberry jam', price: 4.00},
+  {name: 'Mushroom and bean casserole', price: 5.50},
+  {name: 'Chili-flavoured wheat', price: 3.00},
+  {name: 'Vegetarian soup', price: 4.80},
+  {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
+];
 
-let randomNumber =
-  Math.floor(Math.random() * (highNumber - lowNumber + 1)) + lowNumber;
-console.log(randomNumber);
+//1 dunno what this even means
 
-const body = document.querySelector(".textBody");
-body.textContent =
-  "We have selected a random number between " +
-  lowNumber +
-  " and " +
-  highNumber +
-  ". See if you can guess it in 10 turns or fewer. We'll tell you if your guess was too high or too low.";
+// 2 Sort by price
+menus.sort((a,b) => a.price - b.price);
 
-const numbers = document.querySelector(".numbers");
-const times = document.querySelector(".times");
-const lastResult = document.querySelector(".lastResult");
-const lowOrHi = document.querySelector(".lowOrHi");
+console.log("menuSort");
+console.log(menus);
 
-const guessSubmit = document.querySelector(".guessSubmit");
-const guessField = document.querySelector(".guessField");
-const avarage = document.querySelector(".avarage");
+// 3 Filter ( <5€)
+const filterMenu = menus.filter(menu => {
+  return menu.price < 5;
+});
+console.log("filterMenu");
+console.log(filterMenu);
 
-let guessCount = 1;
-let resetButton;
+// 4 Raise price 15% (map)
+const raisePrice = menus.map((menu) => {
+  return{name:menu.name, price:menu.price*1.15};
+});
 
-const startTime = Date.now();
+console.log("raisePrice");
+console.log(raisePrice);
 
-// const checkGuess = () => {
-//   const userGuess = Number(guessField.value);
-//   if (guessCount === 1) {
-//     guesses.textContent = "Previous guesses: ";
-//   }
-//   guesses.textContent += `${userGuess} ` + ", ";
+// 5 Whole menu price
+const sumMenu = menus.reduce((acc, current) => {
+  return acc + current.price;
+}, 0);
 
-//   if (userGuess === randomNumber) {
-//     const endTime = Date.now();
-//     const time = (endTime - startTime) / 1000;
+console.log("sumMenu");
+console.log(sumMenu);
 
-//     lastResult.textContent =
-//       "Congratulations! You got it right! Your time was " +
-//       time +
-//       " seconds. You guessed " +
-//       guessCount +
-//       " times.";
-
-//     lastResult.style.backgroundColor = "#81B29A";
-//     lowOrHi.textContent = "";
-//     setGameOver();
-//   } else if (guessCount === 10) {
-//     lastResult.textContent = "!!!GAME OVER!!!";
-//     lowOrHi.textContent = "";
-//     setGameOver();
-//   } else {
-//     lastResult.textContent = "Wrong!";
-//     lastResult.style.backgroundColor = "#E07A5F";
-//     if (userGuess < randomNumber) {
-//       lowOrHi.textContent = "Last guess was too low!";
-//     } else if (userGuess > randomNumber) {
-//       lowOrHi.textContent = "Last guess was too high!";
-//     }
-//   }
-
-//   guessCount++;
-//   guessField.value = "";
-//   guessField.focus();
-// };
-
-// guessSubmit.addEventListener("click", checkGuess);
-// const setGameOver = () => {
-//   guessField.disabled = true;
-//   guessSubmit.disabled = true;
-//   resetButton = document.createElement("button");
-//   resetButton.textContent = "Start new game";
-//   document.body.append(resetButton);
-//   resetButton.addEventListener("click", resetGame);
-// };
-
-// const resetGame = () => {
-//   guessCount = 1;
-
-//   const resetParas = document.querySelectorAll(".resultParas p");
-//   for (const resetPara of resetParas) {
-//     resetPara.textContent = "";
-//   }
-
-//   resetButton.parentNode.removeChild(resetButton);
-
-//   guessField.disabled = false;
-//   guessSubmit.disabled = false;
-//   guessField.value = "";
-//   guessField.focus();
-
-//   randomNumber = Math.floor(Math.random() * 100) + 1;
-// };
-
-// Week 2 Task 1
-// Always pick a number that is middle of the lowest / highest number
-
-const algGuess = (highNumber, lowNumber) => {
-  //for guessing
-  let highPos = highNumber;
-  let lowPos = lowNumber;
-  let timesGuessed = 0;
-  let numbersGuessed = [];
-  //for alg loop
-  let runAlgTime = 10;
-  let guessCount = [];
-
-  let guess = parseInt(Math.floor((lowPos + highPos) / 2));
-
-  //loop for how many time to run
-  for (let i = 0; i < runAlgTime; i++) {
-    //loop for guessing the number
-    while (true) {
-      if (guess === randomNumber) {
-        numbersGuessed.push(guess);
-        timesGuessed++;
-        guessCount.push(timesGuessed);
-        if (runAlgTime === 1) {
-          lastResult.textContent = "Congratulations! The code works!";
-          lastResult.style.backgroundColor = "#81B29A";
-          times.textContent = "Total time guessed: " + `${timesGuessed} `;
-          numbers.textContent += numbersGuessed;
-        }
-        break;
-      } else {
-        //add new high / low numbers
-        timesGuessed++;
-        if (guess < randomNumber) {
-          lowPos = guess;
-        } else if (guess > randomNumber) {
-          highPos = guess;
-        }
-      }
-      //save number, new guess
-      numbersGuessed.push(guess);
-      guess = parseInt(Math.floor((lowPos + highPos) / 2));
-    }
-    //reset numbers for new round
-    highPos = highNumber;
-    lowPos = lowNumber;
-    timesGuessed = 0;
-    numbersGuessed = [];
-    randomNumber =
-      Math.floor(Math.random() * (highNumber - lowNumber + 1)) + lowNumber;
-  }
-
-  if (runAlgTime > 1) {
-    console.log(guessCount);
-    let max = Math.max(...guessCount);
-    console.log(max);
-    let min = Math.min(...guessCount);
-    console.log(min);
-
-    let average = parseInt(guessCount.reduce((a, b) => a + b) / guessCount.length);
-
-    avarage.textContent =
-      "Avarage it took " + average
-      +" tries. Most it took " +
-      max +
-      " tries. At least it took " +
-      min +
-      " tries.";
-      avarage.style.backgroundColor = "#81B29A";
-  }
-};
-
-console.log(algGuess(highNumber, lowNumber));
-
-//noup
